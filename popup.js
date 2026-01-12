b8ab124396d2178e70c7be5f73bb30f2d746c4ea
GirivarshanR <girivarshanr@gmail.com>
2026-01-12 11:52:06 +0530

    memory button

--- popup.js
+++ popup.js
@@ -1,4 +1,34 @@
-const tgbtn = document.getElementById('tg');
-tgbtn.addEventListener("click", (event) => {
-    event.target.classList.toggle("on");
+document.addEventListener("DOMContentLoaded", () => {
+  const tgbtn = document.getElementById("tg");
+
+  function render(value) {
+    if (value === 1) {
+      tgbtn.classList.add("on");
+      tgbtn.textContent = "Start";
+    } else {
+      tgbtn.classList.remove("on");
+      tgbtn.textContent = "Stop";
+    }
+  }
+  chrome.storage.local.get(["clickedValue"], (result) => {
+    const value = result.clickedValue ?? 0;
+    render(value);
+  });
+
+  tgbtn.addEventListener("click", () => {
+    chrome.storage.local.get(["clickedValue"], (result) => {
+      let clickedValue = result.clickedValue ?? 0;
+      clickedValue = clickedValue === 0 ? 1 : 0;
+
+      chrome.storage.local.set({ clickedValue }, () => {
+        render(clickedValue);
+      });
+    });
+  });
 });
+
+
+
+
+
+
